f5c3c5f7392baab73b5928eeb4ef07da78ca31fc
theaftaab <aftaabhussaint@gmail.com>
2024-07-20 15:03:19 +0530

    Adding test file

--- test.py
+++ test.py
@@ -1,15 +1,20 @@
-from CattleScanner.Cattle_inference import CattleWeight
-from CattleScanner.Cattle_inference import CattleRumination
-import CattleScanner
 import os
 
+import CattleScanner
+from CattleScanner.Cattle_inference import CattleRumination
+from CattleScanner.Cattle_inference import CattleWeight
+
 path = os.path.dirname(CattleScanner.__file__)
+
+############################## Weight inference ############################
 weight_model = CattleWeight(cwd=path)
 side_img = "/Users/aftaabhussain/Work/Data Accumulator and prediction/Images/Side/127_b4-1_s_131_M.jpg"
 rear_img = "/Users/aftaabhussain/Work/Data Accumulator and prediction/Images/Rear/127_b4-1_r_131_M.jpg"
 
 weight = weight_model.predict(side_img, rear_img)
 print(f"The weight is {weight} kg")
+
+############################## rumination inference ############################
 input_video_path1 = "samples/rumination.mp4"
 input_video_path2 = "samples/rumination2.mp4"
 
