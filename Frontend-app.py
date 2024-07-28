c52527cc0a0034c1668eb5d5211ef6f13a780eb2
theaftaab <aftaabhussaint@gmail.com>
2024-07-29 00:56:17 +0530

    Correcting BCS function

--- Frontend-app.py
+++ Frontend-app.py
@@ -39,12 +39,14 @@ rear_image_uploaded = st.file_uploader("Upload Rear Image (jpg, jpeg, png)")
 
 def predict_bcs(side_img_path: str):
     bcs_model = YOLO("CattleScanner/bcs.pt")
-    bcs_classes = ['BCS-1', 'BCS-1.25', 'BCS-1.5', 'BCS-1.75', 'BCS-2', 'BCS-2.25', 'BCS-2.5', 'BCS-2.75', 'BCS-3.0',
-                   'BCS-3.25', 'BCS-3.5', 'BCS-3.75', 'BCS-4.0',
-                   'BCS-4.25', 'BCS-4.5', 'BCS-4.75', 'BCS-5']
+
     results = bcs_model(side_img_path)
-    class_id = int(results[0].boxes.cls.cpu())
-    return bcs_classes[class_id]
+    class_name = None
+    for box in results[0].boxes:
+        class_id = int(box.cls)  # Assuming 'cls' gives the class id
+        class_name = bcs_model.names[class_id]  # Get class name from the model's names dictionary
+
+    return class_name
     # return "BCS-2.25"
 
 
